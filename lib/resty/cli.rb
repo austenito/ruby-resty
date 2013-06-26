8604c57a10a980adc75906f6748eabfdfd0aef20
Austen Ito <austen.ito@gmail.com>
2013-06-26 10:46:24 -0400

    host option is now mandatory.

--- lib/resty/cli.rb
+++ lib/resty/cli.rb
@@ -5,16 +5,17 @@ module Resty
 
     def run
       p = Trollop::Parser.new do
-        opt :host, "The hostname. Ex: http://google.com", type: :string
-        opt :headers, "The headers. Ex: http://google.com", type: :strings, short: "-h"
+        opt :host, "The hostname of the REST service. Ex: http://nyan.cat", type: :string, short: "-h"
+        opt :headers, "The headers sent with each request. Ex: X-NYAN-CAT-SECRET-KEY=nyan_nyan",
+            type: :strings, short: "-H"
         opt :verbose, "Verbose mode", short: "-v"
       end
 
-      options = Trollop::with_standard_exception_handling p do
-        raise Trollop::HelpNeeded if ARGV.empty?
-        p.parse ARGV
+      Trollop::with_standard_exception_handling p do
+        options = p.parse(ARGV)
+        raise Trollop::HelpNeeded if options.empty? || options[:host].nil?
+        Resty::Repl.start(options)
       end
-      Resty::Repl.start(options)
     end
   end
 end
